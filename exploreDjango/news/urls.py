72b605999710a7e87e1a8252142b45a9403a0680
kl1mmm <klimovilya07@mail.ru>
2023-04-03 18:50:54 +0300

    Gets detailView for objects

--- exploreDjango/news/urls.py
+++ exploreDjango/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
+    path(r'^(?P<pk>[0-9]+)/$', views.NewsDetailView.as_view(), name='news-detail'),
 ]
