04b9385107f840dbadbda7537c55fdb943e12963
kl1mmm <klimovilya07@mail.ru>
2023-04-03 11:58:17 +0300

    Data from db are published on news page.

--- exploreDjango/news/views.py
+++ exploreDjango/news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Arctiles
 
 
 # Create your views here.
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Arctiles.objects.order_by('date')
+    return render(request, 'news/news_home.html', {'news': news})
